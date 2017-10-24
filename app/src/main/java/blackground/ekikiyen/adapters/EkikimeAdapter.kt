f7876a0876695a71a2e2e1284d267600c011f6df
Degreat Yartey <yarteydegreat2@gmail.com>
2017-10-24 08:39:46 +0000

    Improved ui for ekikime item

--- app/src/main/java/blackground/ekikiyen/adapters/EkikimeAdapter.kt
+++ app/src/main/java/blackground/ekikiyen/adapters/EkikimeAdapter.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import blackground.ekikiyen.R
 import blackground.ekikiyen.data.Ekikime
 import blackground.ekikiyen.databinding.ViewEkikimeItemBinding
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
 
 class EkikimeAdapter(private val items: ArrayList<Ekikime> = ArrayList()) : RecyclerView.Adapter<EkikimeAdapter.ViewHolder>() {
     override fun getItemCount(): Int = items.size
@@ -37,8 +39,16 @@ class EkikimeAdapter(private val items: ArrayList<Ekikime> = ArrayList()) : Recy
     class ViewHolder(val binding: ViewEkikimeItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(ekikime: Ekikime) {
             binding.ekikimeItem = ekikime
-            binding.timeAgo.setText("")
-            binding.usage.text = "used ${ekikime.usage} times already"
+            binding.timeAgo.setText("5 minutes ago")
+
+            val usageRemaining = 3 - ekikime.usage
+
+            // create drawable for times remaining
+            val colorGenerator = ColorGenerator.MATERIAL
+            val textDrawable = TextDrawable.builder()
+                    .buildRound(usageRemaining.toString(), colorGenerator.randomColor)
+
+            binding.usageRemaining.setImageDrawable(textDrawable)
         }
     }
 }
